46783492c48270f750047f252964c947f5ea1ca7
BRKLK <108090244+BRKLK@users.noreply.github.com>
2025-12-29 16:48:21 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,15 +1,11 @@
-def main():
-    inp = input()
-    n = len(inp)
-    stack = [inp[0]]
+n = int(input())
+inp = input()
+stack = [inp[0]]
 
-    for i in range(1, n):
-        if (len(stack) == 0) or (stack[-1] != inp[i]):
-            stack.append(inp[i])
-        else:
-            stack.pop()
+for i in range(1, n):
+	if (len(stack) == 0) or (stack[-1] != inp[i]):
+		stack.append(inp[i])
+	else:
+		stack.pop()
 
-    return (stack == [])
-
-if __name__ == "__main__":
-    print(main())
+print(1 if stack == [] else 0)
